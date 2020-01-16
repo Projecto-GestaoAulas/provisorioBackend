d28a6cd860fa530bfeafd9678915f3c57c1bff56
Sofia <lucas.a.sofia@gmail.com>
2020-01-16 17:55:35 +0000

    init academy

--- src/main/java/javaGestaoAulas/models/Academy.java
+++ src/main/java/javaGestaoAulas/models/Academy.java
@@ -1,5 +1,34 @@
 package javaGestaoAulas.models;
 
-public class Academy {
+public class Academy extends Entity_ <T> {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String name;
+	private String type;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Academy [name=" + name + ", type=" + type + "]";
+	}
+	
+	
 
 }
